3673cbe24033c12336128c546f2c1fea596d983e
Kanteev Leonid <lkolt2@mail.ru>
2016-03-18 11:55:35 +0300

    Update five.scala

--- five.scala
+++ five.scala
@@ -1,7 +1,10 @@
-  def reversList[A](ls: List[A]) : List[A] = {
-    if (ls.isEmpty){
-      List()
-    } else {
-      reversList(ls.tail) ::: List(ls.head)
+  def reversList[A](ls: List[A]) = {
+    def loop[A](ls: List[A], acc: List[A]) : List[Any] = {
+      if (ls.isEmpty){
+        acc
+      } else {
+        loop(ls.tail, List(ls.head) ::: acc)
+      }
     }
+    loop(ls, List())
   }
